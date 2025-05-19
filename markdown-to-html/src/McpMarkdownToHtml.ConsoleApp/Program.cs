65c107bab55d0043abf28031da348355db5291df
Justin Yoo <justin.yoojh@gmail.com>
2025-05-19 18:08:50 +0900

    Add MCP servers for MD to HTML

---
+++ markdown-to-html/src/McpMarkdownToHtml.ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+
+using McpMarkdownToHtml.Common.Configurations;
+using McpMarkdownToHtml.Common.Extensions;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Configuration
+       .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+       .AddEnvironmentVariables();
+
+builder.Logging.AddConsole(options =>
+{
+    options.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+builder.Services.AddAppSettings(builder.Configuration, args);
+
+builder.Services.AddMcpServer()
+                .WithStdioServerTransport()
+                .WithToolsFromAssembly(Assembly.GetAssembly(typeof(AppSettings)) ?? Assembly.GetExecutingAssembly());
+
+await builder.Build().RunAsync();
+
+
+
+
