a58c297f61ac039bcd0e607cb00bbaf21e47ad64
Justin Yoo <justin.yoojh@gmail.com>
2025-07-17 22:16:06 +0900

    Add an MCP server for Awesome Copilot

---
+++ awesome-copilot/src/McpAwesomeCopilot.ConsoleApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+
+using McpAwesomeCopilot.ConsoleApp.Services;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Logging.AddConsole(options =>
+{
+    options.LogToStandardErrorThreshold = LogLevel.Trace;
+});
+
+var options = new JsonSerializerOptions
+{
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    WriteIndented = true,
+    AllowTrailingCommas = true,
+    PropertyNameCaseInsensitive = true
+};
+builder.Services.AddSingleton(options);
+
+builder.Services.AddHttpClient<IMetadataService, MetadataService>();
+
+builder.Services.AddMcpServer()
+                .WithStdioServerTransport()
+                .WithToolsFromAssembly();
+
+await builder.Build().RunAsync();
