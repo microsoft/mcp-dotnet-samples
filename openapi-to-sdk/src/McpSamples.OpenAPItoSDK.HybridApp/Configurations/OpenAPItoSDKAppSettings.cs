bc44121f00ebbf9439843c08f5cdc365714e2b55
x-or-b <xorb0608@knu.ac.kr>
2025-10-09 18:26:56 +0900

    Refactor namespace and class names for consistency in OpenAPI to SDK settings

--- openapi-to-sdk/src/McpSamples.OpenAPItoSDK.HybridApp/Configurations/OpenAPItoSDKAppSettings.cs
+++ openapi-to-sdk/src/McpSamples.OpenAPItoSDK.HybridApp/Configurations/OpenAPItoSDKAppSettings.cs
@@ -2,12 +2,12 @@ using McpSamples.Shared.Configurations;
 
 using Microsoft.OpenApi.Models;
 
-namespace McpSamples.OpenAPItoSDK.HybridApp.Configurations;
+namespace McpSamples.OpenApiToSdk.HybridApp.Configurations;
 
 /// <summary>
-/// This represents the application settings for markdown-to-html app.
+/// This represents the application settings for openapi-to-sdk app.
 /// </summary>
-public class OpenAPItoSDKAppSettings : AppSettings
+public class OpenApiToSdkAppSettings : AppSettings
 {
     /// <inheritdoc />
     public override OpenApiInfo OpenApi { get; set; } = new()
@@ -16,3 +16,4 @@ public class OpenAPItoSDKAppSettings : AppSettings
         Version = "1.0.0",
         Description = "A simple MCP server for integrating Kiota to generate an SDK from OpenAPI documents."
     };
+}
