90cd98d2840773bfbaf103127f1f3645c1562a8b
seockhyeonkim <krlatjrgus@naver.com>
2025-10-13 05:25:31 +0900

    feat #101: Add PptLoadService to open PPTX

--- ppt-translator/Program.cs
+++ ppt-translator/Program.cs
@@ -1,1 +1,14 @@
-//summary
+using System;
+using McpSamples.PptTranslator.HybridApp.Services;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "TestFiles/sample.pptx";
+        var pptService = new PptLoadService();
+
+        int slideCount = pptService.GetSlideCount(filePath);
+        Console.WriteLine($"슬라이드 수: {slideCount}");
+    }
+}
