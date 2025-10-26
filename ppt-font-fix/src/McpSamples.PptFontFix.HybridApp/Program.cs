3e1c0f45bec5c19cc7d78580f1f2857f057846db
jewelry14 <rlagyqlsdl12@naver.com>
2025-10-26 22:03:14 +0900

    feat(ppt-font-fix): Add ppt file for test to open ppt file

--- ppt-font-fix/src/McpSamples.PptFontFix.HybridApp/Program.cs
+++ ppt-font-fix/src/McpSamples.PptFontFix.HybridApp/Program.cs
@@ -15,10 +15,14 @@ class Program
         });
         ILogger<PPTFontFixService> logger = loggerFactory.CreateLogger<PPTFontFixService>();
 
+        // test.pptx file is normal ppt file for testing
+        // corrupt_test.pptx file is a corrupted ppt file for testing
         string testFilePath = "test.pptx";
+        string corruptTestFilePath = "corrupt_test.pptx";
 
         var pptService = new PPTFontFixService(logger);
 
         await pptService.OpenPPTFileAsync(testFilePath);
+        await pptService.OpenPPTFileAsync(corruptTestFilePath);
     }
 }
